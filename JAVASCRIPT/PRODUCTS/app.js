43493b034d1fa911db56542c2226b69f5205a980
Barbara Maina <160487693+agbarbie@users.noreply.github.com>
2025-02-14 15:05:28 +0300

    Add files via upload

---
+++ JAVASCRIPT/PRODUCTS/app.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("http://localhost:3000/products")
+        .then(response => response.json())
+        .then(products => {
+            const productList = document.getElementById("product-list");
+
+            products.forEach(product => {
+                const productDiv = document.createElement("div");
+                productDiv.classList.add("product");
+
+                productDiv.innerHTML = `
+                    <h2>${product.name}</h2>
+                    <p id="Category"><strong>Category:</strong> ${product.category}</p>
+                    <p id="Description">${product.description}</p>
+                    <p id="price"><strong>Price:</strong> Ksh ${product.price}</p>
+                    <button id="btn"><strong>Stock:</strong> ${product.stock}</p>
+                `;
+                productList.appendChild(productDiv);
+            });
+        })
+        .catch(error => console.error("Error fetching products:", error));
+});
+
