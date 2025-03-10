a78e7ae4f110b57988bd0041842e40a862cd2fe1
agbarbie <barbarawangui2002@gmail.com>
2025-03-10 18:22:57 +0300

    Resolve merge conflicts in TypeScript configuration and package files

--- TYPESCRIPT/3.Unions-Literals/jest.config.js
+++ TYPESCRIPT/3.Unions-Literals/jest.config.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
   };
-=======
-module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-  };
->>>>>>> 41d7ab369c9e98849911c632695ea5c1838075e5
   
